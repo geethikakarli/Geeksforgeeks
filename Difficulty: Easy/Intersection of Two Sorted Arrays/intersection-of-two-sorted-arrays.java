433823cfa90b0fd72f4449c23c415d0f4224faaa
Geethika Karli <karligeethika@gmail.com>
2026-04-09 08:21:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection of Two Sorted Arrays/intersection-of-two-sorted-arrays.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> intersection(int[] a, int[] b) {
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        int i = 0, j = 0;
+        
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j]) {
+                // add only if not duplicate
+                if (res.isEmpty() || res.get(res.size() - 1) != a[i]) {
+                    res.add(a[i]);
+                }
+                i++;
+                j++;
+            } 
+            else if (a[i] < b[j]) {
+                i++;
+            } 
+            else {
+                j++;
+            }
+        }
+        
+        return res;
+    }
+}
