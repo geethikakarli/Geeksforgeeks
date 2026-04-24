855b90e3a8937cc545655a087dc4610a21e98305
Geethika Karli <karligeethika@gmail.com>
2026-04-24 15:04:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Simple Interest/simple-interest.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+
+class Solution {
+    double simpleInterest(int P, int R, int T) {
+        double a = (double) (P*R*T)/100;
+        return a;
+        
+    }
+}
