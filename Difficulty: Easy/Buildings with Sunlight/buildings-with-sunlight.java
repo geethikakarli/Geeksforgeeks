f63cfc1316926bf3e856ec9feb33394540a8d649
Geethika Karli <karligeethika@gmail.com>
2026-04-24 14:09:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Buildings with Sunlight/buildings-with-sunlight.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int visibleBuildings(int arr[]) {
+        int build=0, ptr=arr[0];
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>=ptr){
+                build++;
+                ptr= arr[i];
+            }
+        }
+        
+        return build;
+    }
+}
+ 
