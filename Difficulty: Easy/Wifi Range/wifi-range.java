73af625ebf6d338c56b9bd60ddc4305848283224
Geethika Karli <karligeethika@gmail.com>
2026-05-27 08:50:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Wifi Range/wifi-range.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean wifiRange(String s, int x) {
+        // code here
+        //difference array technique
+        int n = s.length();
+    
+        int[] diffArr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            if(s.charAt(i) == '1'){
+                diffArr[Math.max(0 , i - x)] += 1;
+                if(i + x + 1 < n){
+                    diffArr[i + x + 1] -= 1;
+                }
+            }
+        }
+        //cummulative sum
+        for(int i = 1 ; i < n ; i++){
+            diffArr[i] += diffArr[i-1];
+        }
+        for(int i = 0 ; i < n ; i++){
+            if(diffArr[i] == 0) return false;
+        }
+        
+        return true;
+    }
+}
