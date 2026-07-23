c7d86d4adb650aeafcb3c3f132611798fab848e6
Geethika Karli <karligeethika@gmail.com>
2026-07-23 22:19:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.java
@@ -0,0 +1,21 @@
+class Solution {
+    
+    public void helper(Node root, ArrayList<Integer> preOrder){
+        if(root == null){
+            return;
+        }
+        
+        preOrder.add(root.data);
+        helper(root.left, preOrder);
+        helper(root.right, preOrder);
+    }
+    
+    public ArrayList<Integer> preOrder(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        return ans;
+    }
+}
+
+
+
